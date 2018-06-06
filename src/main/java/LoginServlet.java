73f7087253a2054e05c63c3475f2b829a226b19c
jordilaforge <8899ph@web.de>
2018-06-06 21:53:08 +0200

    added login endpoint

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,15 +3,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Objects;
 
 public class LoginServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setStatus(200);
-        response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
-        out.print("{\"hello\":\"world\"}");
-        out.flush();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (Objects.equals(username, "admin") && Objects.equals(password, "password")) {
+            response.setStatus(200);
+            response.setContentType("application/json");
+            PrintWriter out = response.getWriter();
+            out.print("{\"username\":\"admin\"}");
+            out.flush();
+        } else {
+            response.setStatus(401);
+        }
     }
 }
